6fa4b4e00a4edaa9dfba457204fd11e437826075
Sonu Kumar <sonukumar31815@gmail.com>
2025-03-08 23:28:59 +0530

    [Sonu]Add. uc6

--- Employee_Wage_System/EmployeeWageSystem.js
+++ Employee_Wage_System/EmployeeWageSystem.js
@@ -1,16 +1,20 @@
-// UC 5
-const MAX_HOURS = 160;
-const MAX_DAYS = 20;
-let totalEmpHours = 0;
-let totalDays = 0;
+const WAGE_PER_HOUR = 20;
+const WORKING_DAYS = 20;
+let dailyWages = [];
 
-while (totalEmpHours < MAX_HOURS && totalDays < MAX_DAYS) {
-    let empCheck = Math.floor(Math.random() * 3);
-    totalEmpHours += getWorkHours(empCheck);
-    totalDays++;
+function getWorkHours(empCheck) {
+    switch (empCheck) {
+        case 1: return 4;
+        case 2: return 8;
+        default: return 0;
+    }
 }
 
-let totalWage = totalEmpHours * WAGE_PER_HOUR;
-console.log("Total Employee Wage: $" + totalWage);
+for (let day = 1; day <= WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 3);
+    let dailyHours = getWorkHours(empCheck);
+    let dailyWage = dailyHours * WAGE_PER_HOUR;
+    dailyWages.push(dailyWage);
+}
 
-
+console.log("Daily Wages Array:", dailyWages);
