f6da9915b180f2175d11fbda9fd1f51c9eeae7f7
Sonu Kumar <sonukumar31815@gmail.com>
2025-03-08 23:24:26 +0530

    [Sonu]Add. uc4

--- Employee_Wage_System/EmployeeWageSystem.js
+++ Employee_Wage_System/EmployeeWageSystem.js
@@ -1,14 +1,14 @@
 
-function getWorkHours(empCheck) {
-    switch (empCheck) {
-        case 1: return 4;
-        case 2: return 8;
-        default: return 0;
-    }
+const WAGE_PER_HOUR = 20;
+const WORKING_DAYS = 20;
+
+let totalEmpHours = 0;
+
+for (let day = 0; day < WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 3);
+    totalEmpHours += getWorkHours(empCheck);
 }
 
-const WAGE_PER_HOUR = 20;
-let empCheck = Math.floor(Math.random() * 3); 
-let empHours = getWorkHours(empCheck);
-let empWage = empHours * WAGE_PER_HOUR;
-console.log("Employee Wage: $" + empWage);
+let monthlyWage = totalEmpHours * WAGE_PER_HOUR;
+console.log("Total Employee Wage for the Month: $" + monthlyWage);
+
